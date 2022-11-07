962ada6848c608e3d52ab0889b0be3a7f035d951
Chandra Irawan <chandra.irawan17@gmail.com>
2022-11-07 10:17:36 +0700

    update 6 nove 2022 dan kadaluarsa

---
+++ webapps/index.php
@@ -0,0 +1,18 @@
+<?php
+ header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+ header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT"); 
+ header("Cache-Control: no-store, no-cache, must-revalidate"); 
+ header("Cache-Control: post-check=0, pre-check=0", false);
+ header("Pragma: no-cache"); // HTTP/1.0
+?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="css/default.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="conf/validator.js"></script>
+</head>
+<body>
+  
+<center><font size='30' color='FF55AA' face='Tahoma'>SIMKES Khanza</font></center>
+
+</body>
+
